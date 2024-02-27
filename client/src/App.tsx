6724e9adb6d6b72b54bbc2efdce97c9a297508b0
Ahmed Alsaedi <aalsaedi265@gmail.com>
2024-02-27 08:31:46 -0500

    iterating through todos

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,20 +2,33 @@
 // import { useState } from 'react'
 import './App.css'
 import useSWR from 'swr'
-import { Box } from '@mantine/core'
+import { Box, List } from '@mantine/core'
 import AddToDo from './components/AddTodo'
+
+export interface Todo{
+  id: number,
+  title: string,
+  body: string,
+  done: boolean
+}
 
 export const ENDPOINT = 'http://localhost:4000'
 const fetcher = (url: string) => fetch(`${ENDPOINT}/${url}`).then(r => r.json())
 
 function App() {
 
-  const { data, mutate } = useSWR('api/todos', fetcher)
+  const { data, mutate } = useSWR<Todo[]>('api/todos', fetcher)
   console.log(mutate)
   return (
     <Box>
-      {JSON.stringify(data)}
-      <AddToDo/>
+      <List spacing="xs" size="sm" mb={12} center>
+        {data?.map((todo) => {
+          return <List.Item key={`todo__${todo.id}`}>
+            {todo.title}
+          </List.Item>
+        })}
+      </List>
+      <AddToDo mutate={ mutate} />
     </Box>
   )
 }
--- client/src/components/AddTodo.tsx
+++ client/src/components/AddTodo.tsx
@@ -1,12 +1,12 @@
 
 import { useState } from "react";
-import { useForm } from "@mantine/hooks";
+import { useForm } from "@mantine/form";
 import { Button, Modal, Group, TextInput, Textarea } from "@mantine/core";
 import { ENDPOINT, Todo } from "../App";
 import { KeyedMutator } from "swr";
 
-function AddTodo() {
-    const [open, setOpen] = useState(false)
+function AddTodo({ mutate }: { mutate: KeyedMutator<Todo[]> }) {
+  const [open, setOpen] = useState(false);
 
     const form = useForm({
         initialValues: {
@@ -15,7 +15,19 @@ function AddTodo() {
         },
     });
 
-    async function createTodo(){}
+    async function createTodo(values: {title: string, body:string}) {
+        const update = await fetch(`${ENDPOINT}/api/todos`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(values)
+        }).then(r => r.json())
+
+        mutate(update)
+        form.reset()
+        setOpen(false)
+    }
 
     return (
     <>
@@ -25,11 +37,27 @@ function AddTodo() {
                 title = "basic list"
             >
                 <form onSubmit={form.onSubmit(createTodo)}>
+                    <TextInput
+                        required
+                        mb={12}
+                        label="ToDo"
+                        placeholder="objective to complete"
+                        {...form.getInputProps("title")}
+                    />
+                    <Textarea
+                        mb={12}
+                        label="Body"
+                        placeholder="expand on objective"
+                        {...form.getInputProps("body")}
+                    >
 
+                    </Textarea>
+
+                    
                 </form>
             </Modal>
 
-            <Group position="center">
+            <Group align="center">
                 <Button fullWidth mb={12} onClick={() => setOpen(true)}>
                     Add to List
                 </Button>
